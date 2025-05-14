42204828ba829af6b431b83822a353ea223b0d51
windlliu <windlliu@tencent.com>
2025-05-14 14:05:22 +0800

    AI

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,66 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
-    @State private var tipPercentage = 20
-    @FocusState private var amountIsFocused: Bool
-    
-    let tipPercentages = [10, 15, 20, 25, 0]
-    
-    var totalPerPerson: Double {
-        let tipSelection = Double(tipPercentage)
-        
-        let tipValue = checkAmount / 100 * tipSelection
-        let grandTotal = checkAmount + tipValue
-        let amountPerPerson = grandTotal / Double(numberOfPeople)
-        
-        return amountPerPerson
-    }
-    
-    var body: some View {
-        NavigationStack {
-            Form {
-                Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                        .keyboardType(.decimalPad)
-                        .focused($amountIsFocused)
-                    
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        // 此处以id为值, 默认以索引为值
-                        ForEach(2..<100, id: \.self) {
-                            Text("\($0) people")
-                        }
-                    }.pickerStyle(.navigationLink)
-                }
-                
-                Section("How much do you want to tip?") {
-                    Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
-                            Text($0, format: .percent)
-                        }
-                    }.pickerStyle(.segmented)
-                }
-                
-                Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                }
+  @State private var checkAmount = 0.0
+  @State private var numberOfPeople = 2
+  @State private var tipPercentage = 20
+  @FocusState private var amountIsFocused: Bool
+
+  let tipPercentages = [10, 15, 20, 25, 0]
+
+  var totalPerPerson: Double {
+    let tipSelection = Double(tipPercentage)
+
+    let tipValue = checkAmount / 100 * tipSelection
+    let grandTotal = checkAmount + tipValue
+    let amountPerPerson = grandTotal / Double(numberOfPeople)
+
+    return amountPerPerson
+  }
+
+  var body: some View {
+    NavigationStack {
+      Form {
+        Section {
+          TextField(
+            "Amount", value: $checkAmount,
+            format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+          )
+          .keyboardType(.decimalPad)
+          .focused($amountIsFocused)
+
+          Picker("Number of people", selection: $numberOfPeople) {
+            // 此处以id为值, 默认以索引为值
+            ForEach(2..<100, id: \.self) {
+              Text("\($0) people")
             }
-            .navigationTitle("WeSplit")
-            .toolbar {
-                if amountIsFocused {
-                    Button("Done!") {
-                        amountIsFocused = false
-                    }
-                }
-            }
+          }.pickerStyle(.navigationLink)
         }
+
+        Section("How much do you want to tip?") {
+          Picker("Tip percentage", selection: $tipPercentage) {
+            ForEach(tipPercentages, id: \.self) {
+              Text($0, format: .percent)
+            }
+          }.pickerStyle(.segmented)
+        }
+
+        Section {
+          Text(
+            totalPerPerson,
+            format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+        }
+      }
+      .navigationTitle("WeSplit2")
+      .toolbar {
+        if amountIsFocused {
+          Button("Done!") {
+            amountIsFocused = false
+          }
+        }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  static var previews: some View {
+    ContentView()
 
-    }
+  }
 }
--- WeSplit/WeSplit/today_1.swift
+++ WeSplit/WeSplit/today_1.swift
@@ -8,65 +8,65 @@
 import SwiftUI
 
 struct ContentView2: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selected = "Harry"
-    @State private var tapCount = 0
-    @State private var name = ""
-    
-    var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-//            Text("Hello, world!")
-//        }
-//        .padding()
-        
-//        NavigationStack {
-//            Form { // 表单
-//                Section { // 分块
-//                    Text("Hello ")
-//                    Text("Hello ")
-//                    Text("Hello ")
-//                    Text("Hello ")
-//                    Text("Hello ")
-//                }
-//
-//                Section {
-//                    Text("Hello ")
-//                    Text("Hello ")
-//                    Text("Hello ")
-//                    Text("Hello ")
-//                }
-//
-//                Section {
-//                    Text("Hello ")
-//                    Text("Hello ")
-//                    Text("Hello ")
-//                    Text("Hello ")
-//                }
-//            }
-//            .navigationTitle("SwiftUI")
-//            .navigationBarTitleDisplayMode(.inline)
-//        }
-        
-//        Button("tap Count: \(tapCount)") {
-//            tapCount += 1
-//        }
-        
-//        Form {
-//            TextField("Enter", text: $name)
-//            Text("Hello, \(name)")
-//        }
-        
-        NavigationStack {
-            Form {
-                Picker("Selected Your Name", selection: $selected) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
-                    }
-                }
-            }.navigationTitle("Selected")
+  let students = ["Harry", "Hermione", "Ron"]
+  @State private var selected = "Harry"
+  @State private var tapCount = 0
+  @State private var name = ""
+
+  var body: some View {
+    //        VStack {
+    //            Image(systemName: "globe")
+    //                .imageScale(.large)
+    //                .foregroundColor(.accentColor)
+    //            Text("Hello, world!")
+    //        }
+    //        .padding()
+
+    //        NavigationStack {
+    //            Form { // 表单
+    //                Section { // 分块
+    //                    Text("Hello ")
+    //                    Text("Hello ")
+    //                    Text("Hello ")
+    //                    Text("Hello ")
+    //                    Text("Hello ")
+    //                }
+    //
+    //                Section {
+    //                    Text("Hello ")
+    //                    Text("Hello ")
+    //                    Text("Hello ")
+    //                    Text("Hello ")
+    //                }
+    //
+    //                Section {
+    //                    Text("Hello ")
+    //                    Text("Hello ")
+    //                    Text("Hello ")
+    //                    Text("Hello ")
+    //                }
+    //            }
+    //            .navigationTitle("SwiftUI")
+    //            .navigationBarTitleDisplayMode(.inline)
+    //        }
+
+    //        Button("tap Count: \(tapCount)") {
+    //            tapCount += 1
+    //        }
+
+    //        Form {
+    //            TextField("Enter", text: $name)
+    //            Text("Hello, \(name)")
+    //        }
+
+    NavigationStack {
+      Form {
+        Picker("Selected Your Name", selection: $selected) {
+          ForEach(students, id: \.self) {
+            Text($0).fontWeight(.heavy)
+          }
         }
+      }.navigationTitle("Selected")
     }
+  }
 }
